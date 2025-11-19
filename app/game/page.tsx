c609cae086e26c34276e1ae7136f4be293c8b328
satriadhm <glorioussatria@gmail.com>
2025-11-20 00:17:36 +0700

    fix

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -44,6 +44,7 @@ export default function GamePage() {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setLeaves(Array.from({ length: 8 }, createLeaf));
   }, [createLeaf]);
 
